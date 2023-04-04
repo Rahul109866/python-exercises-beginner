fa03c2e51376400fd5909f5d426abad8cce4b789
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-04 13:29:58 +0530

    Program to generate band name but using fstring for o/p

---
+++ fstring-bandname.py
@@ -0,0 +1,7 @@
+# program to generate band name using City and animal name and use fstrings to get output
+print("Let us Find you bangin name for your band!!")
+city_name = input("Enter the name of your city\n")
+animal_name = input("Enter the name of your favourite animal\n")
+print(
+    f"Your band name could be {city_name} {animal_name}s \nHope you like your new band name"
+)

c19aa1f511867a015436b99b8bc17413796b1b35
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-04 19:27:34 +0530

    This programs calculates BMI using height and weight

---
+++ bmi-calc.py
@@ -0,0 +1,7 @@
+# this program is to calculate the Body Mass Index of a person with their height and weight as data
+
+height = input("Enter your height in metres: ")
+weight = input("Enter your weight in kilograms: ")
+
+bm_index = float(weight) / (float(height) * float(height))
+print(f"Your Body Mass Index is : {bm_index}!")

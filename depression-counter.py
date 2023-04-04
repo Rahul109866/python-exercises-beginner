950176f82864a9997f6a2cf89f124c86a99be4d2
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-04 20:19:47 +0530

    **line 9** change formula of days to include 365

--- depression-counter.py
+++ depression-counter.py
@@ -6,7 +6,7 @@ print(
 expected_years = int(input("Enter your expected age "))
 age_in_years = int(input("Enter your current age "))
 weeks_left_user = (90 * 52) - (age_in_years * 52)
-days_left_user = (90 * 52 * 7) - (age_in_years * 52 * 7)
+days_left_user = (90 * 365) - (age_in_years * 365)
 months_left_user = (90 * 12) - (age_in_years * 12)
 
 print(

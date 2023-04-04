527a1462efcadd4e10ba09681de65e39bc6bf408
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-04 13:13:19 +0530

    This is a program to swap two variables' data. Filename is scuffed

---
+++ hello-World.py
@@ -0,0 +1,11 @@
+print("\n***This is a simple Python script to switch the values of two variables***")
+print("Enter the values of a and b below ")
+a = input("-> a : ")
+b = input("-> b : ")
+
+c = a
+a = b
+b = c
+
+print("\nAfter the switch\n-> a : " + a + "\n-> b : " + b)
+# print("b: "+ b)

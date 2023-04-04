71e0b5ee3ebe82c06253321accf6bf729a3c2c64
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-04 19:29:11 +0530

    Change Output to display rounded off BMI Number use int() typecast

--- bmi-calc.py
+++ bmi-calc.py
@@ -4,4 +4,4 @@ height = input("Enter your height in metres: ")
 weight = input("Enter your weight in kilograms: ")
 
 bm_index = float(weight) / (float(height) * float(height))
-print(f"Your Body Mass Index is : {bm_index}!")
+print(f"Your Body Mass Index is : {int(bm_index)}!")

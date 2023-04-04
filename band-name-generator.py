0e7ea0d117ced592a93fd653788aebebe9254560
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-04 13:15:32 +0530

    Make formatting changes for increased readability

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,8 +1,11 @@
 print("welcome to the Band Name Generator")
+
 print("What's the name of the city you grew up in?")
 city_name = input()
+
 print("What's your pet's name")
 pet_name = input()
+
 print(
     "Your Band name could be"
     + " "

501875e25c4751d911e2ca41448cdf4bd27a39d1
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-05 08:17:45 +0530

    **line 8** format the end result before printing it using format fnc explicitly

--- tip-calculator.py
+++ tip-calculator.py
@@ -5,4 +5,5 @@ tip_percentage = int(input("Enter the tip percentage. Is it 10%, 12%, or 15%"))
 total_amount = bill_amount * (tip_percentage / 100.00) + bill_amount
 person_number = int(input("Enter the number of people"))
 share_amount = round((total_amount / person_number), 2)
-print(f"Each person's share is {share_amount:.2f}")
+share_formatted = "{:.2f}".format(share_amount)
+print(f"Each person's share is {share_formatted}")

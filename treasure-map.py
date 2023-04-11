9d0a24fc7a513acb018abe7853c5480ebfb1aba8
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-11 10:28:19 +0530

    this program marks a spot on the 3X3 grid map

---
+++ treasure-map.py
@@ -0,0 +1,16 @@
+row1 = ["🧱","️🧱","️🧱"]
+row2 = ["🧱","🧱","️🧱"]
+row3 = ["🧱","🧱","🧱"]
+map=[row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position=input("Enter your position to mark")
+
+position_list=list(position)
+map_row=int(position_list[1])-1
+map_column=int(position_list[0])-1
+
+map[map_row][map_column]='🎁'
+
+print(f"{row1}\n{row2}\n{row3}")
+

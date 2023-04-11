52a8c89a63b44027f738f0bdd17313747d618ad2
Rahul109866 <rahulrajendran1098@gmail.com>
2023-04-11 10:33:02 +0530

    Add comments detailing the script

--- treasure-map.py
+++ treasure-map.py
@@ -1,3 +1,4 @@
+#hidden map declaration
 row1 = ["🧱","️🧱","️🧱"]
 row2 = ["🧱","🧱","️🧱"]
 row3 = ["🧱","🧱","🧱"]
@@ -6,6 +7,8 @@ print(f"{row1}\n{row2}\n{row3}")
 
 position=input("Enter your position to mark")
 
+
+#extraction of user input into usable data
 position_list=list(position)
 map_row=int(position_list[1])-1
 map_column=int(position_list[0])-1
@@ -13,4 +16,6 @@ map_column=int(position_list[0])-1
 map[map_row][map_column]='🎁'
 
 print(f"{row1}\n{row2}\n{row3}")
+
+
 
